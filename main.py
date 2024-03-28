9fbfea52d277d5a4c3de3c4eecc77f459fe0b822
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-03-28 19:15:55 +0530

    testing match url output

--- main.py
+++ main.py
@@ -2,4 +2,11 @@
 Entry point main file for stat-retrieval-riot-api
 stat-retrieval-riot-api/api_handler.py
 """
+import pprint
+
 from api_handler import APIHandler
+
+test = APIHandler("jack j", "EU")
+pprint.pprint(test.get_match_data())
+
+# Still under construction 😊

fd308f9d78d6b7695807b2f9c74b4e57a28c0227
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-03-28 18:02:14 +0530

    minor changes

--- api_handler.py
+++ api_handler.py
@@ -4,6 +4,7 @@ stat-retrieval-riot-api/api_handler.py
 """
 import os
 import dotenv
+import requests
 
 dotenv.load_dotenv()
 
@@ -11,6 +12,8 @@ dotenv.load_dotenv()
 class APIHandler:
 
     __API_KEY = os.getenv('API_KEY')
+    __API_URL = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"
+    # fname%20lname
 
     def __init__(self):
         pass
